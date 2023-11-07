d1b15dcedbdaf116e465e47b920f6cb1fcb34260
azara <yyushahu02@gmail.com>
2023-11-07 20:53:06 +0000

    str_help.c

--- str_help.c
+++ str_help.c
@@ -1,22 +1,82 @@
-#include "simple_shell.h"
+#include "a_shell.h"
 
 /**
- * _strchr - function returns a pointer to the first occurence of the character
- *c in the string s
- * @s: pointer to the string to iterate
- * @c: character to iterate for in the string
+ * _strlen - returns the length of a string
+ * @s: pointer to the string
  *
- * Return: a pointer to the matched character or NULL if the character is not
- *found
+ * Return: length of the string
  */
 
-char *_strchr(const char *s, int c)
+int _strlen(char *s)
 {
-	while (*s != '\0')
+	if (*s != '\0')
+		return (1 + _strlen(s + 1));
+	else
+		return (0);
+}
+
+#include "a_shell.h"
+
+/**
+ * _strcspn - a function that returns the length of a segment of a string that
+ * does not contain any of the chars in the specified set
+ * @s: pointer to the string to check
+ * @reject: pointer to the specified set
+ *
+ * Return: length of string not in specified set
+ */
+
+size_t _strcspn(const char *s, const char *reject)
+{
+	size_t char_index, len = 0;
+
+	while (s[len] != '\0')
 	{
-		if (*s == c)
-			return (char *)s; /* cast to char * to match strchr */
-		s++;
+		char_index = 0;
+		while (reject[char_index] != '\0')
+		{
+			if (s[len] == reject[char_index])
+				return (len);
+			char_index++;
+		}
+		len++;
 	}
-	return (NULL);
+	return (len);
+}
+
+/**
+ * _strdup - returns a pointer to a newly alloacted space in memory, which
+ * contains a copy of the string given as a parameter
+ * @s: pointer to the string
+ *
+ * Return: pointer to duplicated string on success
+ */
+
+char *_strdup(char *s)
+{
+	int i, j;
+	char *array;
+
+	if (!s)
+		return (NULL);
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+
+	array = malloc(i * sizeof(char) + 1);
+	if (!array)
+	{
+		free(array);
+		return (NULL);
+	}
+
+	j = 0;
+	while (j < i)
+	{
+		array[j] = s[j];
+		j++;
+	}
+	array[i] = '\0';
+	return (array);
 }
